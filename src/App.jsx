72e9cefce2dbe96f797899e7393141a606cf1168
aidanhenning <henningaidan@gmail.com>
2025-03-18 10:15:04 -0700

    setting up the table and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <ContactList />
     </>
-  )
+  );
 }
 
 export default App
